212acda4f1496337695f382cd8fd3bed183d81c0
dmitrii.sopelnik <dimasix.100@gmail.com>
2025-12-10 17:25:27 +0300

    upd get_data

--- get_data.py
+++ get_data.py
@@ -4,12 +4,17 @@ import gdown
 
 
 def download_data():
-    url = "https://drive.google.com/uc?id=1G_ORPSc2bDB9iSC3AqaBQbrII0I1boTS"
-    out = "data/dataset.parquet"
+    # Если потребуется полный датасет
+    # url = "https://drive.google.com/uc?id=1G_ORPSc2bDB9iSC3AqaBQbrII0I1boTS"
+    # out = "data/dataset.parquet"
+
+    # Сэмпл (10% всех данных)
+    url = "https://drive.google.com/uc?id=19GHV5Ne-sJ1PQOb7hJ0-o3CQhElfFIBn"
+    out = "data/sample.parquet"
     os.makedirs("data", exist_ok=True)
     gdown.download(url, out, quiet=False)
 
 
 if __name__ == "__main__":
     out = download_data()
-    print(f"data saved to {out}")
+    print("Data saved")
